67670c86e381ff6006df88e5b9a5030c8d28c7a4
Sneha M Antonious <snea913@gmail.com>
2024-04-13 11:16:15 -0400

    Initial code

---
+++ src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(fizzBuzz(i));
+
+        }
+    }
+
+    public static String fizzBuzz(int num) {
+        if (num == 1 || num == 2) {
+            return String.valueOf(num);
+        }
+        return null;
+    }
+}
